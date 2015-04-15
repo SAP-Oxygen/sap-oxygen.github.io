d95208e890438f6f4119b06b8e8a081e288e9b5f
unknown <na-da-zerg@hotmail.com>
2015-04-15 16:34:14 -0700

    Fix the gadget not rendering

--- react_opensocial/react_status/react_status.js
+++ react_opensocial/react_status/react_status.js
@@ -9,6 +9,60 @@ var StatusBox = React.createClass({displayName: "StatusBox",
     )
   }
 });
+var StatusBox = React.createClass({displayName: "StatusBox",
+  render: function() {
+    return (
+      React.createElement("div", {className: "StatusBox"}, 
+        React.createElement("div", {className: "page-header"}, 
+          React.createElement("h1", null, "OpenSocial Status")
+        )
+      )
+    )
+  }
+});
+
+var WaveStatus = React.createClass({displayName: "WaveStatus",
+  render: function() {
+    return (
+      React.createElement("div", {className: "WaveStatus"}, 
+        React.createElement("div", {className: "alert alert-success", role: "alert"}, 
+          React.createElement("b", null, "Wave Status:"), " GOOD" 
+        )
+      )
+    )
+  }
+});
+
+var AppdataStatus = React.createClass({displayName: "AppdataStatus",
+  render: function() {
+    return (
+      React.createElement("div", {className: "AppdataStatus"}, 
+        React.createElement("div", {className: "alert alert-success", role: "alert"}, 
+          React.createElement("b", null, "Appdata Status:"), " GOOD"
+        )
+      )
+    )
+  }
+});
+
+var PrefsStatus = React.createClass({displayName: "PrefsStatus",
+  render: function() {
+    return (
+      React.createElement("div", {className: "PrefsStatus"}, 
+        React.createElement("div", {className: "alert alert-danger", role: "alert"}, 
+          React.createElement("b", null, "Prefs Status: "), " BAD"
+        )
+      )
+    )
+  }
+});
+
+gadgets.util.registerOnLoadHandler(function() {
+  React.render(
+    React.createElement(StatusBox),
+    document.getElementById('content')
+    );
+});
 
 var WaveStatus = React.createClass({displayName: "WaveStatus",
   render: function() {
@@ -44,4 +98,11 @@ var PrefsStatus = React.createClass({displayName: "PrefsStatus",
       )
     )
   }
+});
+
+gadgets.util.registerOnLoadHandler(function() {
+  React.render(
+    React.createElement(StatusBox),
+    document.getElementById('content')
+    );
 });
--- react_opensocial/react_status/react_status.jsx
+++ react_opensocial/react_status/react_status.jsx
@@ -14,7 +14,7 @@ var WaveStatus = React.createClass({
   render: function() {
     return (
       <div className="WaveStatus">
-        <div class="alert alert-success" role="alert">
+        <div className="alert alert-success" role="alert">
           <b>Wave Status:</b> GOOD 
         </div>
       </div>
@@ -26,7 +26,7 @@ var AppdataStatus = React.createClass({
   render: function() {
     return (
       <div className="AppdataStatus">
-        <div class="alert alert-success" role="alert">
+        <div className="alert alert-success" role="alert">
           <b>Appdata Status:</b> GOOD
         </div>
       </div>
@@ -38,10 +38,17 @@ var PrefsStatus = React.createClass({
   render: function() {
     return (
       <div className="PrefsStatus">
-        <div class="alert alert-danger" role="alert">
+        <div className="alert alert-danger" role="alert">
           <b>Prefs Status: </b> BAD
         </div>
       </div>
     )
   }
+});
+
+gadgets.util.registerOnLoadHandler(function() {
+  React.render(
+    React.createElement(StatusBox),
+    document.getElementById('content')
+    );
 });
