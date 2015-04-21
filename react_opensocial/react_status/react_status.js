aa681bf5c2e7de18d6f0922d70aaf9fa6c3742f7
unknown <na-da-zerg@hotmail.com>
2015-04-20 17:35:11 -0700

    Start implementing AppdataStatus class

--- react_opensocial/react_status/react_status.js
+++ react_opensocial/react_status/react_status.js
@@ -26,48 +26,6 @@ var WaveStatus = React.createClass({displayName: "WaveStatus",
   },
   componentDidMount: function() {
     console.log("inComponentDidMount");
-    
-    var self = this;
-    var loops = 5;
-    var delay = 1500;
-    var testData = {};
-    testData["test"] = {test: "test"};
-    var color;
-    var waveStateStatus;
-
-    for (i = 0; i < loops; i++) {
-      if (wave.getState()) {
-        waveStateStatus = true;
-      } else {
-        waveStateStatus = false;
-      }
-    }
-    var localData = this.state.data;
-    localData["waveStateStatus"] = waveStateStatus;
-    self.setState(localData);
-    wave.getState().submitDelta(testData);
-    
-    function onWaveUpdate() {
-      var testData = {};
-      testData["test"] = {test: "test"};
-      var waveData = {};
-      var waveState = wave.getState();
-      var submitDeltaStatus;
-      $.each(waveState.getKeys(), function(index, key) {
-        waveData[key] = waveState.get(key);
-      });
-      // JSON method
-      if (JSON.stringify(waveData["test"]) === JSON.stringify(testData["test"])) {
-        submitDeltaStatus = true;
-      } else {
-        submitDeltaStatus = false;
-      }
-      var localData = self.state.data;
-      localData["submitDeltaStatus"] = submitDeltaStatus;
-      self.setState(localData);
-    }
-
-    wave.setStateCallback(onWaveUpdate);
   },
   handleTest: function(e) {
     e.preventDefault();
@@ -75,8 +33,6 @@ var WaveStatus = React.createClass({displayName: "WaveStatus",
     console.log("inHandleTest");
 
     var self = this;
-    var loops = 5;
-    var delay = 1500;
     var testData = {};
     testData["test"] = {test: "test"};
     var color;
@@ -141,11 +97,45 @@ var WaveStatus = React.createClass({displayName: "WaveStatus",
 });
 
 var AppdataStatus = React.createClass({displayName: "AppdataStatus",
+  getInitialState: function() {
+    console.log("inGetInitialState");
+    return {data: {}};
+  },
+  componentDidMount: function() {
+    console.log("inComponentDidMount");
+  },
+  handleTest: function(e) {
+    e.preventDefault();
+
+    var appdata_get = function() {
+      osapi.appdata.get({userId: '@viewer', groupId: '@self', keys: ["test"]}).execute(function (userData) {
+        console.log("appdata_get: " + gadgets.json.stringify(data));
+        return data;
+      });
+    };
+
+    var appdata_update = function(input){
+      osapi.appdata.update({userId: '@viewer', groupId: '@self', data: {test: input}}).execute(function (userData) {
+        if (userData.error) {
+          console.log("appdata_update failed");
+        }
+        else {
+          console.log("appdata_update succeeded");
+        }
+      });
+    };
+
+    appdata_update("test");
+    appdata_get();
+  },
   render: function() {
     return (
       React.createElement("div", {className: "AppdataStatus"}, 
         React.createElement("div", {className: "alert alert-success", role: "alert"}, 
           React.createElement("b", null, "Appdata Status:"), " GOOD"
+        ), 
+        React.createElement("button", {type: "button", className: "btn btn-default btn-sm", onClick: this.handleTest}, 
+         "Test"
         )
       )
     );
--- react_opensocial/react_status/react_status.jsx
+++ react_opensocial/react_status/react_status.jsx
@@ -26,48 +26,6 @@ var WaveStatus = React.createClass({
   },
   componentDidMount: function() {
     console.log("inComponentDidMount");
-    
-    var self = this;
-    var loops = 5;
-    var delay = 1500;
-    var testData = {};
-    testData["test"] = {test: "test"};
-    var color;
-    var waveStateStatus;
-
-    for (i = 0; i < loops; i++) {
-      if (wave.getState()) {
-        waveStateStatus = true;
-      } else {
-        waveStateStatus = false;
-      }
-    }
-    var localData = this.state.data;
-    localData["waveStateStatus"] = waveStateStatus;
-    self.setState(localData);
-    wave.getState().submitDelta(testData);
-    
-    function onWaveUpdate() {
-      var testData = {};
-      testData["test"] = {test: "test"};
-      var waveData = {};
-      var waveState = wave.getState();
-      var submitDeltaStatus;
-      $.each(waveState.getKeys(), function(index, key) {
-        waveData[key] = waveState.get(key);
-      });
-      // JSON method
-      if (JSON.stringify(waveData["test"]) === JSON.stringify(testData["test"])) {
-        submitDeltaStatus = true;
-      } else {
-        submitDeltaStatus = false;
-      }
-      var localData = self.state.data;
-      localData["submitDeltaStatus"] = submitDeltaStatus;
-      self.setState(localData);
-    }
-
-    wave.setStateCallback(onWaveUpdate);
   },
   handleTest: function(e) {
     e.preventDefault();
@@ -75,8 +33,6 @@ var WaveStatus = React.createClass({
     console.log("inHandleTest");
 
     var self = this;
-    var loops = 5;
-    var delay = 1500;
     var testData = {};
     testData["test"] = {test: "test"};
     var color;
@@ -141,12 +97,46 @@ var WaveStatus = React.createClass({
 });
 
 var AppdataStatus = React.createClass({
+  getInitialState: function() {
+    console.log("inGetInitialState");
+    return {data: {}};
+  },
+  componentDidMount: function() {
+    console.log("inComponentDidMount");
+  },
+  handleTest: function(e) {
+    e.preventDefault();
+
+    var appdata_get = function() {
+      osapi.appdata.get({userId: '@viewer', groupId: '@self', keys: ["test"]}).execute(function (userData) {
+        console.log("appdata_get: " + gadgets.json.stringify(data));
+        return data;
+      });
+    };
+
+    var appdata_update = function(input){
+      osapi.appdata.update({userId: '@viewer', groupId: '@self', data: {test: input}}).execute(function (userData) {
+        if (userData.error) {
+          console.log("appdata_update failed");
+        }
+        else {
+          console.log("appdata_update succeeded");
+        }
+      });
+    };
+
+    appdata_update("test");
+    appdata_get();
+  },
   render: function() {
     return (
       <div className="AppdataStatus">
         <div className="alert alert-success" role="alert">
           <b>Appdata Status:</b> GOOD
         </div>
+        <button type="button" className="btn btn-default btn-sm" onClick={this.handleTest}>
+         Test
+        </button>
       </div>
     );
   }
