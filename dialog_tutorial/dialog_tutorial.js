151d6940da27c56a8b2691320aa06c9794e1bae0
unknown <na-da-zerg@hotmail.com>
2015-05-06 16:17:54 -0700

    Add checkbox for each row item

--- dialog_tutorial/dialog_tutorial.js
+++ dialog_tutorial/dialog_tutorial.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
   $("#button").click(function() {
     var item = $('<tr></tr>');
-    var col1 = $('<input>').attr({
+    var col1 = $('<td></td>').append($('<input>').attr({
       type: "checkbox",
       name: "done"
-    });
+    }));
     var col2 = $('<td></td>').text('col2');
     var col3 = $('<td></td>').text('col3');
     item.append(col1, col2, col3);
