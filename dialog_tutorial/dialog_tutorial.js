09b2193a7fcf69eca5b16bae10a40fe308c562b0
unknown <na-da-zerg@hotmail.com>
2015-05-06 14:17:01 -0700

    Change onclick function

--- dialog_tutorial/dialog_tutorial.js
+++ dialog_tutorial/dialog_tutorial.js
@@ -1,8 +1,9 @@
-var createDummy = function() {
-  $("#table").append( $( "
-      <tr>
-        <td><input type="checkbox" name="vehicle" value="Bike"></td>
-        <td>thing</td>
-        <td>5/20</td>
-      </tr>"));
-};
+$(document).ready(function() {
+  $("#button").click(function() {
+    $("#table").append("<tr>
+                          <td><input type="checkbox" name="vehicle" value="Bike"></td>
+                          <td>thing</td>
+                          <td>5/20</td>
+                        </tr>");
+  });
+});
