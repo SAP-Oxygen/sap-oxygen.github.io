a764bba69f85eafad0f6efa5f52465138ee6b3fc
unknown <na-da-zerg@hotmail.com>
2015-05-06 16:14:21 -0700

    Alter onclick function

--- dialog_tutorial/dialog_tutorial.js
+++ dialog_tutorial/dialog_tutorial.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
   $("#button").click(function() {
     var item = $('<tr></tr>');
-    var col1 = $('<td></td>').text('col1');
+    var col1 = $('<input>').attr({
+      type: "checkbox",
+      name: "done"
+    });
     var col2 = $('<td></td>').text('col2');
     var col3 = $('<td></td>').text('col3');
     item.append(col1, col2, col3);
