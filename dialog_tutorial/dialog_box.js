60b073999e8683b8a5af9eae469e7ed383177210
unknown <na-da-zerg@hotmail.com>
2015-05-07 13:49:55 -0700

    Add a warning message for input fields

--- dialog_tutorial/dialog_box.js
+++ dialog_tutorial/dialog_box.js
@@ -2,7 +2,15 @@ $(document).ready(function() {
   $("#submit-btn").click(function() {
     var task = $("#task").val();
     var due = $("#due").val();
-    gadgets.views.setReturnValue({task: task, due: due});
-    gadgets.views.close();
+    if(task === "" && due === ""){
+      $("#message").attr("style", "color: red;").text("Please enter valid task and due");
+    } else if(task === ""){
+      $("#message").attr("style", "color: red;").text("Please enter a valid task");
+    } else if(due === ""){
+      $("#message").attr("style", "color: red;").text("Please enter a valid due");
+    } else {
+      gadgets.views.setReturnValue({task: task, due: due});
+      gadgets.views.close();
+    }
   });
 });
