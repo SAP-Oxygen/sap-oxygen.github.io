d8910cbcf5c90c0cab79df93ec728fd5726d3154
unknown <na-da-zerg@hotmail.com>
2015-05-07 14:06:50 -0700

    Refactor addItem method

--- dialog_tutorial/dialog_tutorial.js
+++ dialog_tutorial/dialog_tutorial.js
@@ -1,32 +1,34 @@
 $(document).ready(function() {
   $("#button").click(function() {
-    var item = $('<tr></tr>');
-    var col1 = $('<td></td>').append($('<input>').attr({
-      type: "checkbox",
-      name: "done"
-    }));
-    var col2 = $('<td></td>').text('col2');
-    var col3 = $('<td></td>').text('col3');
-    item.append(col1, col2, col3);
-
-    $("#table").append(item);
+    addItem("task1", "due1");
   });
 
   $("#open-dialog-btn").click(function() {
     gadgets.views.openGadget(function(result){
-      var item = $('<tr></tr>');
-      var col1 = $('<td></td>').append($('<input>').attr({
-        type: "checkbox",
-        name: "done"
-      }));
-      var col2 = $('<td></td>').text(result["task"]);
-      var col3 = $('<td></td>').text(result["due"]);
-      item.append(col1, col2, col3);
-
-      $("#table").append(item);
+      addItem(result["task"], result["due"]);
       gadgets.window.adjustHeight();
     }, 
     function(site){}, 
     {view: "dialog",viewTarget: "DIALOG"});
   });
 });
+
+var addItem(task, due) {
+  var item = $('<tr></tr>');
+  var col1 = $('<td></td>').append($('<input>').attr({
+    type: "checkbox",
+    name: "done"
+  }));
+  var col2 = $('<td></td>').text(task);
+  var col3 = $('<td></td>').text(due);
+  item.append(col1, col2, col3);
+
+  $("#table").append(item);
+  gadgets.window.adjustHeight();
+}
+
+// var init = function() {
+
+// };
+
+// gadgets.util.registerOnLoadHandler(init)
