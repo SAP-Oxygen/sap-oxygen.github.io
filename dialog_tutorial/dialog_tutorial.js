3bfaef097d72b87eeb41a4fa0d7ae73afcac65ba
unknown <na-da-zerg@hotmail.com>
2015-05-08 11:33:38 -0700

    Fix typo

--- dialog_tutorial/dialog_tutorial.js
+++ dialog_tutorial/dialog_tutorial.js
@@ -31,7 +31,7 @@ var createRow = function(item) {
 var createList = function(items) {
   var table = $('<table></table>').attr({
     id: "init-table",
-    style: "width: 100%");
+    style: "width: 100%"});
   var header = $('<tr></tr>');
   var col1 = $('<td></td>').text("Done");
   var col2 = $('<td></td>').text("Todo");
