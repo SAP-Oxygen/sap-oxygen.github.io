711d0e77f2d88d70afbd70fe8557a2b08434e540
unknown <na-da-zerg@hotmail.com>
2015-05-19 11:07:52 -0700

    Modify initial_data

--- GY/agenda_gadget/cstar_agenda.js
+++ GY/agenda_gadget/cstar_agenda.js
@@ -9,6 +9,6 @@ CSTAR.agenda.readOnly = false;
 CSTAR.agenda.itemUuid = "1";
 CSTAR.agenda.ctlId = "1";
 CSTAR.agenda.acURL = "";
-CSTAR.agenda.initial_data = {};
+CSTAR.agenda.initial_data = {items: [{id: "1", id: "2", id: "3"}], startTime: 0};
 CSTAR.agenda.widget = null ;
 CSTAR.agenda.militaryTime = false;
