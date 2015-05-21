4cdec9818323d2dd8bce6980ac801042d134c655
Joseph Wong <joseph.wong@sap.com>
2015-05-21 11:41:28 -0700

    jsquared

---
+++ jsquared/jsquared.js
@@ -0,0 +1,53 @@
+(function($) {
+  var GroupList = React.createClass({displayName: "GroupList",
+    // Standard React API
+    render: function() {
+      var self = this;
+      var prefs = new gadgets.Prefs();
+
+      return (
+        React.createElement("div", null, 
+          React.createElement("div", null, prefs.getMsg('title')), 
+          React.createElement("ul", null, 
+          
+            $.map(this.getAllGroups(), function(group, index) {
+              return (React.createElement("li", {key: group.Id}, group.Name));
+            })
+          
+          )
+        )
+      );
+    },
+    getInitialState: function() {
+      return {data: {d: {results: []}}, users: []};
+    },
+    componentDidMount: function() {
+      this.loadGroups();
+      gadgets.window.adjustHeight();
+    },
+    componentDidUpdate: function(prevProps, prevState) {
+      gadgets.window.adjustHeight();
+    },
+    // Our own code
+    loadGroups: function() {
+      gadgets.io.makeRequest("http://localhost:3000/api/v1/OData/Groups?$format=json",
+        function(result) {
+          this.setState({data: result, users: this.state.users});
+        },
+        {
+          AUTHORIZATION: 'OAUTH2',
+          OAUTH2_SERVICE_NAME: 'g1'
+        });
+    },
+    getGroups: function() {
+      this.state.data.d.results;
+    }
+  });
+
+  gadgets.util.registerOnLoadHandler(function() {
+    React.render(
+      React.createElement(GroupList, null),
+      document.body
+    );
+  });
+})(jQuery);
---
+++ jsquared/jsquared.jsx
@@ -0,0 +1,53 @@
+(function($) {
+  var GroupList = React.createClass({
+    // Standard React API
+    render: function() {
+      var self = this;
+      var prefs = new gadgets.Prefs();
+
+      return (
+        <div>
+          <div>{prefs.getMsg('title')}</div>
+          <ul>
+          {
+            $.map(this.getAllGroups(), function(group, index) {
+              return (<li key={group.Id}>{group.Name}</li>);
+            })
+          }
+          </ul>
+        </div>
+      );
+    },
+    getInitialState: function() {
+      return {data: {d: {results: []}}, users: []};
+    },
+    componentDidMount: function() {
+      this.loadGroups();
+      gadgets.window.adjustHeight();
+    },
+    componentDidUpdate: function(prevProps, prevState) {
+      gadgets.window.adjustHeight();
+    },
+    // Our own code
+    loadGroups: function() {
+      gadgets.io.makeRequest("http://localhost:3000/api/v1/OData/Groups?$format=json",
+        function(result) {
+          this.setState({data: result, users: this.state.users});
+        },
+        {
+          AUTHORIZATION: 'OAUTH2',
+          OAUTH2_SERVICE_NAME: 'g1'
+        });
+    },
+    getGroups: function() {
+      this.state.data.d.results;
+    }
+  });
+
+  gadgets.util.registerOnLoadHandler(function() {
+    React.render(
+      <GroupList/>,
+      document.body
+    );
+  });
+})(jQuery);
