57371f0870abc1a379db3e36ec034176958d96f6
Marc Bell <marc.bell@sap.com>
2015-06-26 10:05:21 -0700

    debugging

--- testing_MB/Google_Hangouts_sameWindow/Google_Hangouts_sameWindow.js
+++ testing_MB/Google_Hangouts_sameWindow/Google_Hangouts_sameWindow.js
@@ -1,7 +1,9 @@
+gapi.hangout.render('placeholder-div', { 'render': 'createhangout' });
+
 // Initializes gadget, runs scripts
-function init() {
-    gapi.hangout.render('placeholder-div', { 'render': 'createhangout' });
-}
+//function init() {
+    //gapi.hangout.render('placeholder-div', { 'render': 'createhangout' });
+//}
 
 // Initializes gadget after receiving a notification that the page is loaded and the DOM is ready.
-gadgets.util.registerOnLoadHandler(init);
+//gadgets.util.registerOnLoadHandler(init);
