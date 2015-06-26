160f2292c32251ab4fa3e2c296ac85baa0fc0db0
Marc Bell <marc.bell@sap.com>
2015-06-25 18:27:37 -0700

    Initial Commit

---
+++ testing_MB/Google_Hangouts_sameWindow/Google_Hangouts_sameWindow.js
@@ -0,0 +1,7 @@
+// Initializes gadget, runs scripts
+function init() {
+    gapi.hangout.render('placeholder-div', { 'render': 'createhangout' });
+}
+
+// Initializes gadget after receiving a notification that the page is loaded and the DOM is ready.
+gadgets.util.registerOnLoadHandler(init);
