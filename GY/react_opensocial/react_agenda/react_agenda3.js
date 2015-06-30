0aa00cb78b4f0c8679a24e05426cdba6eb533d8a
unknown <na-da-zerg@hotmail.com>
2015-06-30 15:07:12 -0700

    wrap react.render around gadget.onloadhandler

--- GY/react_opensocial/react_agenda/react_agenda3.js
+++ GY/react_opensocial/react_agenda/react_agenda3.js
@@ -500,4 +500,6 @@ var DATA = {
   { id: 5, text: 'enhancement' }, { id: 6, text: 'bug' }, { id: 7, text: 'duplicate' }, { id: 8, text: 'invalid' }, { id: 9, text: 'wontfix' }]
 }
 
-React.render(React.createElement(Agenda, {data: DATA}), document.body);
+gadgets.util.registerOnLoadHandler(function() {
+  React.render(React.createElement(Agenda, {data: DATA}), document.body);
+});
