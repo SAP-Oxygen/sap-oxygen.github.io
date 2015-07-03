fe74128b28c3f14c3a00bb4f75e47322bb39d717
Marc Bell <marc.bell@sap.com>
2015-07-03 15:44:58 -0700

    Fixing Cross scripting

--- testing_MB/IoT/GoogleMapsTest/GoogleMapsTest.js
+++ testing_MB/IoT/GoogleMapsTest/GoogleMapsTest.js
@@ -3,5 +3,5 @@
   <ModulePrefs title="Google Maps Test - Heat Map" height="600">
         <Require feature="dynamic-height"/>
     </ModulePrefs>
-    <Content type="html" href="GoogleMapsTest.html"/>
+    <Content type="url" href="https://www.google.com/maps/place/Waterfront+Stn+West+Unload+Only/@49.2729987,-123.1250285,15z/data=!4m2!3m1!1s0x5486717809bde70d:0x9ade3b8ff2a4e400"/>
 </Module>
