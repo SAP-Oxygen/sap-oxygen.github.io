b5abf364c447e7415329a1709c93b8596722155f
Joseph Wong <joseph.wong@sap.com>
2015-07-04 11:48:47 -0700

    street view

--- snappy/snappy.js
+++ snappy/snappy.js
@@ -1,15 +1,13 @@
 function initialize() {
-  var fenway = new google.maps.LatLng(42.345573, -71.098326);
+  var currentPosition = new google.maps.LatLng(42.345573, -71.098326);
   var mapOptions = {
-    center: fenway,
+    center: currentPosition,
     zoom: 14
   };
-  var map = new google.maps.Map(
-      document.getElementById('map-canvas'), mapOptions);
+  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
   var panoramaOptions = {
-    position: fenway,
+    position: currentPosition,
     pov: {
-      heading: 34,
       pitch: 10
     }
   };
@@ -26,9 +24,10 @@ function initialize() {
     if (places.length == 0) {
         return;
     }
-    
+
     var pos = places[0].geometry.location;
     map.setCenter(pos);
+    map.setStreetView(panorama);
   });
 
   gadgets.window.adjustHeight();
