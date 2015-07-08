9e035269fbc87aa38138b968940d9d7363554d54
unknown <na-da-zerg@hotmail.com>
2015-07-08 16:18:05 -0700

    make basic template for dialog box

--- GY/react_opensocial/react_agenda/react_agenda_dialog_box5.js
+++ GY/react_opensocial/react_agenda/react_agenda_dialog_box5.js
@@ -1,17 +1,24 @@
 var Input = ReactBootstrap.Input;
 var Row = ReactBootstrap.Row;
 var Col = ReactBootstrap.Col;
+var ButtonInput = ReactBootstrap.ButtonInput;
+var Button = ReactBootstrap.Button;
 
 var DialogBox = React.createClass({displayName: "DialogBox",
+  handleSubmit: function(e){
+    e.preventDefault();
+  },
   render: function() {
     return(
       React.createElement("div", null, 
         React.createElement("h3", null, 
         "Add New Agenda Topic"
         ), 
-        React.createElement("form", null, 
+        React.createElement("form", {onSubmit: this.handlesubmit}, 
           React.createElement(Input, {type: "text", label: "Topic", placeholder: "Enter Agenda Topic"}), 
-          React.createElement(Input, {type: "textarea", label: "Details", placeholder: "Describe This Topic"})
+          React.createElement(Input, {type: "textarea", label: "Details", placeholder: "Describe This Topic"}), 
+          React.createElement(ButtonInput, {type: "submit", bsSize: "small"}, "Submit"), 
+          React.createElement(ButtonInput, {bsSize: "small"}, "Close")
         )
       )
     );
--- GY/react_opensocial/react_agenda/react_agenda_dialog_box5.jsx
+++ GY/react_opensocial/react_agenda/react_agenda_dialog_box5.jsx
@@ -1,17 +1,24 @@
 var Input = ReactBootstrap.Input;
 var Row = ReactBootstrap.Row;
 var Col = ReactBootstrap.Col;
+var ButtonInput = ReactBootstrap.ButtonInput;
+var Button = ReactBootstrap.Button;
 
 var DialogBox = React.createClass({
+  handleSubmit: function(e){
+    e.preventDefault();
+  },
   render: function() {
     return(
       <div>
         <h3>
         Add New Agenda Topic
         </h3>
-        <form>
+        <form onSubmit={this.handlesubmit}>
           <Input type='text' label='Topic' placeholder='Enter Agenda Topic' />
           <Input type='textarea' label='Details' placeholder='Describe This Topic' />
+          <ButtonInput type='submit' bsSize='small'>Submit</ButtonInput>
+          <ButtonInput bsSize='small'>Close</ButtonInput>
         </form>
       </div>
     );
