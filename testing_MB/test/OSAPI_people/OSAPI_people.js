49dc424a0be7c86b92b0c4e32659be8025c19b9c
MarcBell-SAP <marc.bell@sap.com>
2015-10-09 15:47:33 -0700

    Diagnosing possible bug.

--- testing_MB/test/OSAPI_people/OSAPI_people.js
+++ testing_MB/test/OSAPI_people/OSAPI_people.js
@@ -4,7 +4,7 @@ var urlController = function() {
 		    var displayFrame = $("<iframe id='url-frame' class='url-gadget-display-frame' src=''></iframe>");
 		    var display = $(gadget).find("#display");
 		    
-			function makeODataCall(){
+			/*function makeODataCall(){
 			  gadgets.io.makeRequest("https://developer.sapjam.com/api/v1/OData/Self?$format=json",
 			    function(result) {
 			      console.log("Email Address: " + result.data.d.results.Email);
@@ -18,20 +18,26 @@ var urlController = function() {
 			      OAUTH_SERVICE_NAME: 'gadgetOauth2SAMLBearerFlow',
 			      CONTENT_TYPE: gadgets.io.ContentType.JSON
 			    });
-			}
+			}*/
 
 			function makeOSAPIpeopleCall(){
 				osapi.people.getOwner().execute(function(data) {
-				console.log(data.displayName);
-				console.log(data.email);
-				url = "https://whoknows.com/app/profiles/embed/" + data.email;
-				console.log("navigating to: " + url);
-				displayFrame.slideDown();
-				displayFrame.attr("src", url);
+					console.log(data);
+					console.log(data.id);
+					console.log(data.displayName);
+					console.log(data.givenName);
+					console.log(data.familyName );
+					console.log(data.nickname);
+					console.log(data.email);
+					console.log(data.thumbnailUrl);
+
+					console.log(data.email);
+					console.log(data.displayName);
+					url = "https://whoknows.com/app/profiles/embed/" + data.email;
+					console.log("navigating to: " + url);
+					displayFrame.slideDown();
+					displayFrame.attr("src", url);
 				});
-				/*osapi.people.getOwner().execute(function(data) {
-				    document.getElementById("osapi").innerHTML += "<br>osapi.people.getOwner: " + data.displayName + ".<br>";
-				});*/
 			}
 
 		    function init() {
