5a32cefcdd4c095ea9db6ebd02ac70a4c7d3340e
MarcBell-SAP <marc.bell@sap.com>
2015-10-09 15:58:31 -0700

    Cleaning up the code.

--- testing_MB/test/OSAPI_people/OSAPI_people.js
+++ testing_MB/test/OSAPI_people/OSAPI_people.js
@@ -3,25 +3,10 @@ var urlController = function() {
 		init: function(gadget) {
 		    var displayFrame = $("<iframe id='url-frame' class='url-gadget-display-frame' src=''></iframe>");
 		    var display = $(gadget).find("#display");
-		    
-			/*function makeODataCall(){
-			  gadgets.io.makeRequest("https://developer.sapjam.com/api/v1/OData/Self?$format=json",
-			    function(result) {
-			      console.log("Email Address: " + result.data.d.results.Email);
-			      url = "https://whoknows.com/app/profiles/embed/" + result.data.d.results.Email;
-			      console.log("navigating to: " + url);
-			      displayFrame.slideDown();
-			      displayFrame.attr("src", url);
-			    },
-			    {
-			      AUTHORIZATION: 'OAUTH2',
-			      OAUTH_SERVICE_NAME: 'gadgetOauth2SAMLBearerFlow',
-			      CONTENT_TYPE: gadgets.io.ContentType.JSON
-			    });
-			}*/
 
 			function makeOSAPIpeopleCall(){
 				osapi.people.getOwner().execute(function(data) {
+
 					console.log("Logging everything from osapi.people.getOwner");
 					console.log("---------------------------------------------");
 					console.log("data = " + data);
@@ -33,6 +18,7 @@ var urlController = function() {
 					console.log("data.email = " + data.email);
 					console.log("data.thumbnailUrl = " + data.thumbnailUrl);
 					console.log("---------------------------------------------");
+					
 					url = "https://whoknows.com/app/profiles/embed/" + data.email;
 					console.log("navigating to: " + url);
 					displayFrame.slideDown();
@@ -43,7 +29,6 @@ var urlController = function() {
 		    function init() {
 		    	display.append(displayFrame);
 				makeOSAPIpeopleCall();
-		    	//makeODataCall();
 			}
 
 			init();
