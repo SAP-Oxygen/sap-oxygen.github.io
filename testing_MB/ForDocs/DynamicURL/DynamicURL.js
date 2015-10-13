62816dc388e2b399aacaf987c928a5992cfa1d7c
MarcBell-SAP <marc.bell@sap.com>
2015-10-13 16:01:41 -0700

    Initial Commit

---
+++ testing_MB/ForDocs/DynamicURL/DynamicURL.js
@@ -0,0 +1,21 @@
+var urlController = function() {
+    return {
+		init: function(gadget) {
+		    var displayFrame = $("<iframe id='url-frame' class='url-gadget-display-frame' src=''></iframe>");
+		    var display = $(gadget).find("#display");
+
+			function callRemoteURL(url){
+				console.log("navigating to: " + url);
+				displayFrame.slideDown();
+				displayFrame.attr("src", url);
+			}
+
+		    function init() {
+		    	display.append(displayFrame);
+				callRemoteURL("https://www.sap.com");
+			}
+
+			init();
+		}
+    };
+}();
