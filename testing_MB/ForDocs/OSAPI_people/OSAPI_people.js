e116c29f4b4b2f37affc05851b7eeff6afb9feea
MarcBell-SAP <marc.bell@sap.com>
2015-10-14 16:32:29 -0700

    Applied style changes from getViewer to getOwner

--- testing_MB/ForDocs/OSAPI_people/OSAPI_people.js
+++ testing_MB/ForDocs/OSAPI_people/OSAPI_people.js
@@ -6,19 +6,40 @@ function makeOSAPIpeopleCall(){
 		osapiOutput += "<p>------------------------------------------------------------------------------------------</p>";
 		osapiOutput += "<p><h2>osapi.people.getViewer</h2></p>";
 		osapiOutput += "<ul>";
-	    osapiOutput += "<li>data.<b>displayName</b> = " + data.displayName + "</li>";
-	    osapiOutput += "<li>data.<b>name.givenName</b> = " + data.name.givenName + "</li>";
-	    osapiOutput += "<li>data.<b>name.familyName</b> = " + data.name.familyName + "</li>";
-	    osapiOutput += "<li>data.<b>name.formatted</b> = " + data.name.formatted + "</li>";
-	    osapiOutput += "<li>data.<b>emails[0].type</b> = " + data.emails[0].type + "</li>";
-	    osapiOutput += "<li>data.<b>emails[0].value</b> = " + data.emails[0].value + "</li>";
-	    osapiOutput += "<li>data.<b>thumbnailUrl</b> = " + data.thumbnailUrl + "</li>";
-	    osapiOutput += "<li>data.<b>photos[0].type</b> = " + data.photos[0].type + "</li>";
-	    osapiOutput += "<li>data.<b>photos[0].value</b> = " + data.photos[0].value + "</li>";
-	    osapiOutput += "</ul>";
-	    osapiOutput += "<p>------------------------------------------------------------------------------------------</p>";
-	    osapiOutput += "<p></p>";
-	    $("body").append(osapiOutput);
+		osapiOutput += "<li>data.<b>displayName</b> = " + data.displayName + "</li>";
+		osapiOutput += "<li>data.<b>name.givenName</b> = " + data.name.givenName + "</li>";
+		osapiOutput += "<li>data.<b>name.familyName</b> = " + data.name.familyName + "</li>";
+		osapiOutput += "<li>data.<b>name.formatted</b> = " + data.name.formatted + "</li>";
+		osapiOutput += "<li>data.<b>emails[0].type</b> = " + data.emails[0].type + "</li>";
+		osapiOutput += "<li>data.<b>emails[0].value</b> = " + data.emails[0].value + "</li>";
+		osapiOutput += "<li>data.<b>thumbnailUrl</b> = " + data.thumbnailUrl + "</li>";
+		osapiOutput += "<li>data.<b>photos[0].type</b> = " + data.photos[0].type + "</li>";
+		osapiOutput += "<li>data.<b>photos[0].value</b> = " + data.photos[0].value + "</li>";
+		osapiOutput += "</ul>";
+		osapiOutput += "<p>------------------------------------------------------------------------------------------</p>";
+		osapiOutput += "<p></p>";
+		$("body").append(osapiOutput);
+	});
+
+	osapi.people.getOwner().execute(function(data) {
+		var osapiOutput = "";
+		osapiOutput += "<p></p>";
+		osapiOutput += "<p>------------------------------------------------------------------------------------------</p>";
+		osapiOutput += "<p><h2>osapi.people.getOwner</h2></p>";
+		osapiOutput += "<ul>";
+		osapiOutput += "<li>data.<b>displayName</b> = " + data.displayName + "</li>";
+		osapiOutput += "<li>data.<b>name.givenName</b> = " + data.name.givenName + "</li>";
+		osapiOutput += "<li>data.<b>name.familyName</b> = " + data.name.familyName + "</li>";
+		osapiOutput += "<li>data.<b>name.formatted</b> = " + data.name.formatted + "</li>";
+		osapiOutput += "<li>data.<b>emails[0].type</b> = " + data.emails[0].type + "</li>";
+		osapiOutput += "<li>data.<b>emails[0].value</b> = " + data.emails[0].value + "</li>";
+		osapiOutput += "<li>data.<b>thumbnailUrl</b> = " + data.thumbnailUrl + "</li>";
+		osapiOutput += "<li>data.<b>photos[0].type</b> = " + data.photos[0].type + "</li>";
+		osapiOutput += "<li>data.<b>photos[0].value</b> = " + data.photos[0].value + "</li>";
+		osapiOutput += "</ul>";
+		osapiOutput += "<p>------------------------------------------------------------------------------------------</p>";
+		osapiOutput += "<p></p>";
+		$("body").append(osapiOutput);
 	});
 
 	osapi.people.getViewerFriends().execute(function(data) {
@@ -36,20 +57,6 @@ function makeOSAPIpeopleCall(){
 	        osapiOutput += "<p>" + data.list[i].photos[0].type + ".</p>";
 	        osapiOutput += "<p>" + data.list[i].photos[0].value + ".</p>";
 	    }
-	    $("body").append(osapiOutput);
-	});
-
-	osapi.people.getOwner().execute(function(data) {
-		var osapiOutput = "";
-	    osapiOutput += "<p>osapi.people.getOwner: " + data.displayName + ".</p>";
-	    osapiOutput += "<p>osapi.people.getOwner: " + data.name.givenName + ".</p>";
-	    osapiOutput += "<p>osapi.people.getOwner: " + data.name.familyName + ".</p>";
-	    osapiOutput += "<p>osapi.people.getOwner: " + data.name.formatted + ".</p>";
-	    osapiOutput += "<p>osapi.people.getOwner: " + data.emails[0].type + ".</p>";
-	    osapiOutput += "<p>osapi.people.getOwner: " + data.emails[0].value + ".</p>";
-	    osapiOutput += "<p>osapi.people.getOwner: " + data.thumbnailUrl + ".</p>";
-	    osapiOutput += "<p>osapi.people.getOwner: " + data.photos[0].type + ".</p>";
-	    osapiOutput += "<p>osapi.people.getOwner: " + data.photos[0].value + ".</p>";
 	    $("body").append(osapiOutput);
 	});
 
