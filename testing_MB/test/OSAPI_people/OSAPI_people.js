df16085af2d18461e8047f7848f66b968e783c48
MarcBell-SAP <marc.bell@sap.com>
2015-10-13 18:45:27 -0700

    Added license.

--- testing_MB/test/OSAPI_people/OSAPI_people.js
+++ testing_MB/test/OSAPI_people/OSAPI_people.js
@@ -1,3 +1,13 @@
+/*
+License
+Copyright 2014, SAP AG
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+*/
 var urlController = function() {
     return {
 		init: function(gadget) {
@@ -20,7 +30,6 @@ var urlController = function() {
 					console.log("data.thumbnailUrl = " + data.thumbnailUrl);
 					console.log("---------------------------------------------");
 					
-					// This is here because there's a bug.
 					if (!data.emails[0].value){
 						url = "https://whoknows.com/app/profiles/embed/yourEmailAddress@whoknows.com";
 					}
