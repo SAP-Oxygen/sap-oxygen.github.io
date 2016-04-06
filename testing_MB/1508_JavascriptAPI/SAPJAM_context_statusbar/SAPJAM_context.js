afa1e5497bde1aeee45090d8b3d410f89bb7da71
MarcBell-SAP <marc.bell@sap.com>
2016-04-06 15:04:52 -0700

    Adding more functionality.

--- testing_MB/1508_JavascriptAPI/SAPJAM_context_statusbar/SAPJAM_context.js
+++ testing_MB/1508_JavascriptAPI/SAPJAM_context_statusbar/SAPJAM_context.js
@@ -26,11 +26,42 @@ function make_SAPJAM_context_Call(){
 		/* End HTML page */
 		osapiOutput += ". Open your browser console to view the raw JSON object.";
 
-		/* Highlight the status bar and displays the string inside it (osapiOutput). */
+		/* Must show the following features:
+			-> DONE - gadgets.sapjam.statusbar.show()
+				-> show the current gadget if hidden
+				-> Shows the contents (HTML body) of the statusbar gadget
+				-> Shows the contents of the body element in the gadget
+				-> Expands the gadget and shows the contents of the body element within it
+			-> gadgets.sapjam.statusbar.hide()
+				-> hide the current gadget if shown
+				-> Hides the gadget
+				-> Collapses the gadget
+			-> DONE - gadgets.sapjam.statusbar.highlight()
+				-> change the status bar section for the current gadget to a blue highlight and throb it 3 times
+				-> Highlight the status bar and displays the string inside it (osapiOutput).
+				-> Highlights the gadget in blue and makes it blink 3 times
+			-> gadgets.sapjam.statusbar.clearHighlight()
+				-> remove any blue highlighting on the status bar section for the current gadget
+				-> 
+			-> DONE - gadgets.sapjam.statusbar.setBadgeText(someString)
+				-> add a red badge with the given string next to the gadget's title in the status bar section for the current gadget
+				-> Adds a red badge with the given string to the left of the gadget's title in the status bar of the gadget
+				-> Adds a red badge with text to the gadget
+			-> gadgets.sapjam.statusbar.clearBadgeText()
+				-> remove any red badge in the status bar section for the current gadget
+				-> 
+		*/
+
+		/* Highlights the gadget in blue and makes it blink 3 times */
 		gadgets.sapjam.statusbar.highlight();
+
+		/* Adds a red badge with text to the gadget */
 		gadgets.sapjam.statusbar.setBadgeText("Click here");
 
-		/* Instructs the user how to use the statusbar gadget */
+		/* Expands the gadget and shows the contents of the body element within it */
+		gadgets.sapjam.statusbar.show();
+
+		/* Appends the string (osapiOutput) to the body of the HTML page. */
 		$("body").append(osapiOutput);
 	});
 	
