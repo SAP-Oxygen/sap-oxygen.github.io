a8514d6548cd346eeb2dc206df1ad51845f08a0f
MarcBell-SAP <marc.bell@sap.com>
2016-04-08 15:04:55 -0700

    changed init

--- testing_MB/1508_JavascriptAPI/SAPJAM_context_statusbar/SAPJAM_context.js
+++ testing_MB/1508_JavascriptAPI/SAPJAM_context_statusbar/SAPJAM_context.js
@@ -8,16 +8,13 @@ http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
 */
-var osapiOutput = "";
 
 function make_SAPJAM_context_Call(){
 	gadgets.sapjam.context.get(function(data) {
 		console.log(JSON.stringify(data, null, 4));
 
 		/* Begin HTML page */
-		if (osapiOutput != ""){
-
-		}
+		var osapiOutput = "";
 		osapiOutput += "gadgets.sapjam.context.get --> ";
 
 		/* Adds all the properties of "gadgets.sapjam.context.get" with HTML formatting to a string (osapiOutput). */
@@ -73,14 +70,9 @@ function clearHighlight(){
 	gadgets.sapjam.statusbar.clearHighlight();
 }
 
-// Initializes gadget, sets callbacks
+// Initializes gadget
 function init() {
-	console.log("init");
-    if (wave && wave.isInWaveContainer()) {
-    	console.log("isInWaveContainer");
-    	// Loads the gadget's initial state and the subsequent changes to it
-        wave.setStateCallback(make_SAPJAM_context_Call);
-    }
+	make_SAPJAM_context_Call();
 }
 
 // Initializes gadget after receiving a notification that the page is loaded and the DOM is ready.
