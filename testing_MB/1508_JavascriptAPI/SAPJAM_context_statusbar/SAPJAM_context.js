748157148760234b41c5f0e5594d101ce9b3991b
MarcBell-SAP <marc.bell@sap.com>
2016-04-08 14:23:32 -0700

    Using wave's registerOnLoadHandler to check for the page and gadget being ready instead of JQuery's equivalent which only checks for the page being ready.

--- testing_MB/1508_JavascriptAPI/SAPJAM_context_statusbar/SAPJAM_context.js
+++ testing_MB/1508_JavascriptAPI/SAPJAM_context_statusbar/SAPJAM_context.js
@@ -9,7 +9,6 @@ http://www.apache.org/licenses/LICENSE-2.0
 Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
 */
 function make_SAPJAM_context_Call(){
-
 	gadgets.sapjam.context.get(function(data) {
 		console.log(JSON.stringify(data, null, 4));
 
@@ -26,35 +25,10 @@ function make_SAPJAM_context_Call(){
 		/* End HTML page */
 		osapiOutput += ". Open your browser console to view the raw JSON object.";
 
-		/* Must show the following features:
-			-> DONE - gadgets.sapjam.statusbar.show()
-				-> show the current gadget if hidden
-				-> Shows the contents (HTML body) of the statusbar gadget
-				-> Shows the contents of the body element in the gadget
-				-> Expands the gadget and shows the contents of the body element within it
-			-> DONE - gadgets.sapjam.statusbar.hide()
-				-> hide the current gadget if shown
-				-> Hides the gadget
-				-> Collapses the gadget
-			-> DONE - gadgets.sapjam.statusbar.highlight()
-				-> change the status bar section for the current gadget to a blue highlight and throb it 3 times
-				-> Highlight the status bar and displays the string inside it (osapiOutput).
-				-> Highlights the gadget in blue and makes it blink 3 times
-			-> DONE - gadgets.sapjam.statusbar.clearHighlight()
-				-> remove any blue highlighting on the status bar section for the current gadget
-				-> 
-			-> DONE - gadgets.sapjam.statusbar.setBadgeText(someString)
-				-> add a red badge with the given string next to the gadget's title in the status bar section for the current gadget
-				-> Adds a red badge with the given string to the left of the gadget's title in the status bar of the gadget
-				-> Adds a red badge with text to the gadget
-			-> DONE - gadgets.sapjam.statusbar.clearBadgeText()
-				-> remove any red badge in the status bar section for the current gadget
-				-> 
-		*/
-
 		/* Appends the string (osapiOutput) to the body of the HTML page. */
 		$("body").append(osapiOutput);
 
+		/* Demonstrates every gadgets.sapjam.statusbar interaction */
 		setTimeout(showStatusBar, 3000);
 		setTimeout(clearBadgeText, 6000);
 		setTimeout(hideStatusBar, 9000);
@@ -62,7 +36,6 @@ function make_SAPJAM_context_Call(){
 		setTimeout(clickToExpand, 15000);
 		setTimeout(clearHighlight, 18000);
 	});
-	
 }
 
 function showStatusBar(){
@@ -96,3 +69,13 @@ function clearHighlight(){
 	gadgets.sapjam.statusbar.clearHighlight();
 }
 
+// Initializes gadget, sets callbacks
+function init() {
+    if (wave && wave.isInWaveContainer()) {
+    	// Loads the gadget's initial state and the subsequent changes to it
+        wave.setStateCallback(make_SAPJAM_context_Call);
+    }
+}
+
+// Initializes gadget after receiving a notification that the page is loaded and the DOM is ready.
+gadgets.util.registerOnLoadHandler(init);
